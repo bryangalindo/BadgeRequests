53295f0464a7a3fadf1615cc631a80e3464a5691
Will <56137739+wpank84@users.noreply.github.com>
2021-03-16 16:33:19 -0500

    working on integrating auth on badge routes

--- API/main.py
+++ API/main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from Routers import badges, requests
+from typing import Optional
+from starlette.requests import Request
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.responses import HTMLResponse, JSONResponse, RedirectResponse
+from starlette.config import Config
 
+from authlib.integrations.starlette_client import OAuth
+
+from Data.connection import Table
+from dependencies import get_current_user
 
 app = FastAPI()
 
@@ -17,11 +26,67 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.add_middleware(SessionMiddleware, secret_key='!secret')
 
 app.include_router(badges.router)
 app.include_router(requests.router)
+
+config = Config('.env')
+oauth = OAuth(config)
+
+CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+oauth.register(
+    name='google',
+    server_metadata_url=CONF_URL,
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
+)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get('/test')
+async def home(request: Request):
+    user = request.session.get('user')
+    if user is not None:
+        email = user['email']
+        html = (
+            f'<pre>Email: {email}</pre><br>'
+            '<a href="/docs">documentation</a><br>'
+            '<a href="/logout">logout</a>'
+        )
+        return HTMLResponse(html)
+    return HTMLResponse('<a href="/login">login</a>')
+
+
+@app.get('/login', tags=['authentication'])
+async def login(request: Request):
+    # Redirect Google OAuth back to our application
+    redirect_uri = request.url_for('auth')
+
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+
+
+@app.route('/auth')
+async def auth(request: Request):
+    # Perform Google OAuth
+    token = await oauth.google.authorize_access_token(request)
+    user = await oauth.google.parse_id_token(request, token)
+
+    # Save the user
+    request.session['user'] = dict(user)
+
+    return RedirectResponse(url='/test')
+
+
+# Tag it as "authentication" for our docs
+@app.get('/logout', tags=['authentication'])
+async def logout(request: Request):
+    # Remove the user
+    request.session.pop('user', None)
+
+    return RedirectResponse(url='/test')
